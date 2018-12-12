df1b008d593f81720ed6055497b9f813a3f1199c
Doncho Mitev <doncho.mitev@mail.bg>
2018-12-12 12:30:16 +0200

    create admin controller, viewAllUsers and viewUser actions

--- src/CoffeeShopBundle/Controller/AdminController.php
+++ src/CoffeeShopBundle/Controller/AdminController.php
@@ -12,10 +12,21 @@ class AdminController extends Controller
      * @Route("/admin/all_users", name="all_users")
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction()
+    public function viewAllUsersAction()
     {
         $users = $this->getDoctrine()->getRepository(User::class)->findAll();
 
         return $this->render('admin/all_users.html.twig', ['users' => $users]);
+    }
+
+    /**
+     * @Route("/admin/user/{id}", name="one_user")
+     * @param $id
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function viewOneUser($id) {
+        $user = $this->getDoctrine()->getRepository(User::class)->find($id);
+        return $this->render("admin/user.html.twig", ['user' => $user]);
     }
 }
