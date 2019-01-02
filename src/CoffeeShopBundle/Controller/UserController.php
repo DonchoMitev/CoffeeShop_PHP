d1532e3e141ee6f7a928c10fc8275cae81fe6a12
Doncho Mitev <doncho.mitev@mail.bg>
2019-01-02 09:50:44 +0200

    create profile page and edit profile action

--- src/CoffeeShopBundle/Controller/UserController.php
+++ src/CoffeeShopBundle/Controller/UserController.php
@@ -4,10 +4,13 @@ namespace CoffeeShopBundle\Controller;
 
 use CoffeeShopBundle\Entity\Role;
 use CoffeeShopBundle\Entity\User;
+use CoffeeShopBundle\Form\ProfileEditType;
 use CoffeeShopBundle\Form\UserType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
@@ -59,5 +62,30 @@ class UserController extends Controller
             ->find($userId);
         return $this->render("user/profile.html.twig",
             ['user' => $user]);
+    }
+
+    /**
+     * @Route("/profile/edit", name="user_profile_edit")
+     * @Security(expression="is_granted('IS_AUTHENTICATED_FULLY')")
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function editProfileAction(Request $request)
+    {
+        /** @var User $currentUser */
+        $currentUser = $this->getUser();
+        $form = $this->createForm(ProfileEditType::class, $currentUser);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($currentUser);
+            $em->flush();
+            $this->addFlash("success", "Profile updated!");
+            return $this->redirectToRoute("user_profile");
+        }
+        return $this->render("/user/edit.html.twig", [
+            "edit_form" => $form->createView()
+        ]);
     }
 }
